0ceb480d8e26e59606afa7fa422fe338de0d5bcc
Mayur Gajbhiye <mayurgajbhiye4@gmail.com>
2025-07-16 13:51:16 +0530

    eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,24 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc'
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: import.meta.dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next'],
+    ignorePatterns: [
+      'node_modules/**',
+      '.next/**',
+      'src/generated/**', 
+      '**/.prisma/**',         // Exclude .prisma folders (sometimes used for generated code)
+      '**/prisma/client/**',   // Exclude generated client if present
+    ],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+]
 
-export default eslintConfig;
+export default eslintConfig  
